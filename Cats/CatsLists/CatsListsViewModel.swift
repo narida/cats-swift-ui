aff636f1039fb1556fa26abf62989b334e330263
Elisha Narida <enarida@gmail.com>
2022-06-08 20:40:00 +0800

    rename

--- Cats/CatsLists/CatsListsViewModel.swift
+++ Cats/CatsLists/CatsListsViewModel.swift
@@ -13,10 +13,10 @@ class CatsListsViewModel: ObservableObject {
     
     @Published var cats: [Cat] = []
     
-    private let baseUrl = "https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=true&include_breeds=true"
+    private let url = "https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=true&include_breeds=true"
     
     func getCats() {
-       guard let url = URL(string: baseUrl) else { return }
+        guard let url = URL(string: url) else { return }
        task = NetworkManager.download(url: url)
            .decode(type: [Cat].self, decoder: JSONDecoder())
            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (cats) in
